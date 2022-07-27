7ff1537763930848fc6a99b1bd7c69d0519f9d94
shekharsamal1993 <93323272+shekharsamal1993@users.noreply.github.com>
2022-07-27 17:36:29 +0530

    Add files via upload

---
+++ src/main/java/com/appliedinformatics/cdaapi/mu2parser/ProcedureParser.java
@@ -0,0 +1,41 @@
+package com.appliedinformatics.cdaapi.mu2parser;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import org.openhealthtools.mdht.uml.cda.Procedure;
+import org.openhealthtools.mdht.uml.cda.consol.ProceduresSection;
+
+public class ProcedureParser {
+
+    /**
+     * Parse procedures *
+     */
+    ProceduresSection proceduresSection = null;
+
+    public ProcedureParser(ProceduresSection proceduresSection) {
+        this.proceduresSection = proceduresSection;
+    }
+
+    public ArrayList parse() {
+        ArrayList proc_parsed = new ArrayList<HashMap<String, String>>();
+        //ArrayList proc_parsed = new ArrayList<String>();
+        try {
+            for (Procedure proc : this.proceduresSection.getProcedures()) {
+                HashMap proc_map = new HashMap<String, String>();
+                proc_map.put("code", proc.getCode().getCode());
+                proc_map.put("name", proc.getCode().getDisplayName());
+                proc_map.put("ts", CDAParserUtil.getTS(proc.getEffectiveTime()));
+                proc_map.put("status", proc.getStatusCode().getCode());
+                proc_parsed.add(proc_map);
+            }
+        } catch (Exception ex) {
+            System.out.println("Procedure Parsing Error:");
+            ex.printStackTrace();
+        }
+
+        Collections.sort(proc_parsed, new DateComparator());
+        return proc_parsed;
+    }
+
+}
